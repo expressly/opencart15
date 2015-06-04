10a53f8c76b677ff2a63f95103c3fc34f4997909
Sam Pratt <samj.pratt@gmail.com>
2015-06-04 06:23:09 +0100

    Append additional address properly; cart cleared before user logged in

--- catalog/controller/expressly/migrate.php
+++ catalog/controller/expressly/migrate.php
@@ -19,7 +19,6 @@ class ControllerExpresslyMigrate extends CommonController
         $dispatcher->dispatch('customer.migrate.start', $event);
 
         $this->data['response'] = $event->getResponse();
-
         $this->request->get['route'] = 'common/home';
 
         $this->children = array(
@@ -48,79 +47,108 @@ class ControllerExpresslyMigrate extends CommonController
 
         $event = new CustomerMigrateEvent($this->getMerchant(), $uuid);
         $dispatcher->dispatch('customer.migrate.complete', $event);
-
         $json = $event->getResponse();
 
         if (empty($json)) {
             $this->redirect('/');
         }
 
+        $this->customer->logout();
+        $this->cart->clear();
+
         try {
-            $email = $json['migration']['data']['email'];
+            $email = $json['data']['email'];
             $this->load->model('account/customer');
-            if ($this->model_account_customer->getTotalCustomersByEmail($email) > 0) {
-                $this->redirect('/');
+
+            if ($this->model_account_customer->getTotalCustomersByEmail($email) == 0) {
+                $this->load->model('account/address');
+                $this->load->model('expressly/country');
+                $this->load->model('expressly/zone');
+
+                $password = md5('xly' . microtime());
+                $customer = $json['data']['customerData'];
+                $phone = !empty($customer['phones']) ? $customer['phones'][0]['number'] : '';
+
+                $shippingAddress = $customer['addresses'][$customer['shippingAddress']];
+                $code = $app['country_code.provider']->getIso3($shippingAddress['country']);
+                $country = $this->model_expressly_country->getByIso3($code);
+                $zone = array('zone_id' => 0);
+                if (!empty($country) && !empty($shippingAddress['stateProvince'])) {
+                    $zone = $this->model_expressly_zone->getByNameOrCodeAndCountry($shippingAddress['stateProvince'],
+                        $country['country_id']);
+                }
+
+                /*
+                 * Limitations:
+                 * OpenCart can only use 1 address; use default shipping address
+                 * Payment integration with OpenCart assumed to not use 3d secure,
+                 * which would explain why this hasn't been an issue for more
+                 * than a decade.
+                 * Only 1 phone can be added per user (does not include country code)
+                 * OpenCart forces first and last name on the address, changes below
+                 *
+                 * Adding the billing address first, as the default is set whenever
+                 * a new address is added.
+                 *
+                 * Email is dispatched after core function completes
+                 */
+                $this->model_account_customer->addCustomer(array(
+                    'firstname' => $customer['firstName'],
+                    'lastname' => $customer['lastName'],
+                    'email' => $email,
+                    'password' => $password,
+                    'telephone' => $phone,
+                    'fax' => '',
+                    'newsletter' => true,
+                    'company' => !empty($shippingAddress['company']) ? $shippingAddress['company'] : '',
+                    'company_id' => '',
+                    'tax_id' => !empty($customer['taxNumber']) ? $customer['taxNumber'] : '',
+                    'address_1' => $shippingAddress['address1'],
+                    'address_2' => !empty($shippingAddress['address2']) ? $shippingAddress['address2'] : '',
+                    'city' => $shippingAddress['city'],
+                    'postcode' => $shippingAddress['zip'],
+                    'country_id' => !empty($country) ? $country['country_id'] : '',
+                    'zone_id' => $zone['zone_id']
+                ));
+
+                // Log user in
+                $this->customer->login($email, $password, true);
+
+                /*
+                 * Can only add extra addresses to a logged in user (stupid right?)
+                 * Default billing address added, but not connected
+                 */
+                $billingAddress = $customer['addresses'][$customer['billingAddress']];
+                $code = $app['country_code.provider']->getIso3($billingAddress['country']);
+                $country = $this->model_expressly_country->getByIso3($code);
+                $zone = array('zone_id' => 0);
+                if (!empty($country) && !empty($billingAddress['stateProvince'])) {
+                    $zone = $this->model_expressly_zone->getByNameOrCodeAndCountry($billingAddress['stateProvince'],
+                        $country['country_id']);
+                }
+
+                $this->model_account_address->addAddress(array(
+                    'firstname' => $customer['firstName'],
+                    'lastname' => $customer['lastName'],
+                    'company' => !empty($billingAddress['company']) ? $billingAddress['company'] : '',
+                    'company_id' => '',
+                    'tax_id' => !empty($customer['taxNumber']) ? $customer['taxNumber'] : '',
+                    'address_1' => $billingAddress['address1'],
+                    'address_2' => !empty($billingAddress['address2']) ? $billingAddress['address2'] : '',
+                    'city' => $billingAddress['city'],
+                    'postcode' => $billingAddress['zip'],
+                    'country_id' => !empty($country) ? $country['country_id'] : '',
+                    'zone_id' => $zone['zone_id']
+                ));
             }
-
-            $password = md5('xly' . microtime());
-            $customer = $json['migration']['data']['customerData'];
-            $phone = !empty($customer['phones']) ? $customer['phones'][0]['number'] : '';
-            $shippingAddress = $customer['addresses'][$customer['shippingAddress']];
-            $code = $app['country_code.provider']->getIso3($shippingAddress['country']);
-
-            $this->load->model('expressly/country');
-            $this->load->model('expressly/zone');
-            $country = $this->model_expressly_country->getByIso3($code);
-            $zone = array(
-                'zone_id' => ''
-            );
-            if (!empty($country)) {
-                $zone = $this->model_expressly_zone->getByNameOrCodeAndCountry($shippingAddress['stateProvince'],
-                    $country['country_id']);
-            }
-
-            /*
-             * Limitations:
-             * OpenCart can only use 1 address; use default shipping address
-             * Payment integration with OpenCart assumed to not use 3d secure,
-             * which would explain why this hasn't been an issue for more
-             * than a decade.
-             * Only 1 phone can be added per user (does not include country code)
-             * OpenCart forces first and last name on the address, changes below
-             *
-             * Email is dispatched after core function completes
-             */
-            $this->model_account_customer->addCustomer(array(
-                'firstname' => $customer['firstName'],
-                'lastname' => $customer['lastName'],
-                'email' => $email,
-                'password' => $password,
-                'telephone' => $phone,
-                'newsletter' => true,
-                'company' => $shippingAddress['company'],
-                'company_id' => '',
-                'tax_id' => $customer,
-                'address_1' => $shippingAddress['address1'],
-                'address_2' => $shippingAddress['address2'],
-                'city' => $shippingAddress['city'],
-                'postcode' => $shippingAddress['zip'],
-                'country_id' => !empty($country) ? $country['country_id'] : '',
-                'zone_id' => $zone['zone_id']
-            ));
-            /*
-             * Default billing address added, but not connected
-             */
-
-            // Log user in
-            $this->customer->login($email, $password, true);
 
             // Add item to cart
             if (!empty($json['cart']['productId'])) {
-                $this->cart->add($json['cart']['productId']);
+                $this->cart->add($json['cart']['productId'], 1, '');
             }
 
             if (!empty($json['cart']['couponCode'])) {
-                $this->session->data['coupon'];
+                $this->session->data['coupon'] = $json['cart']['couponCode'];
             }
 
             $dispatcher->dispatch('customer.migrate.success', $event);
